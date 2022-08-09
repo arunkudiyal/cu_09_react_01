a20b6806ca1cca6de0f6719546a5cf55aa932d53
arunkudiyal <arunkudiyal@icloud.com>
2022-08-09 09:11:52 +0530

    Introduction React States

--- src/App.js
+++ src/App.js
@@ -1,22 +1,68 @@
-import './App.css';
-import Button from './Button'
+import { Component } from 'react'
+import Person from './Person/Person';
 
-const App = () => {
-  // JavaSCript
-  const onClickHandler = (e) => {
-    console.log(e)
+class App extends Component {
+  // JavaScript
+  state = {
+    persons: [
+      {name: 'Person - 1', age: 22}, 
+      {name: 'Person - 2', age: 23}, 
+      {name: 'Person - 3', age: 24},
+      {name: 'Person - 4', age: 25},
+    ]
   }
 
-  return (
-    <div className='container'>
-      <h1>Welcome to React!</h1> <hr />
-      <Button contextual="primary" text="Button - 1" />
-      <Button contextual="danger"  />
-      <Button click={(e) => onClickHandler(e)} contextual="success" text="Button - 3" />
-      <Button contextual="info" text="Button - 4" />
-      <Button contextual="warning"  />
-    </div>
-  );
-}
+  onChangeNameHandler = () => {
+    // DONOT MUTATE THE STATES DIRECTLY
+    this.state.persons[1].name = 'Person - 0'
+    this.state.persons[1].age =  '0'
+  }
 
+  render() {
+    // JSX
+    return (
+      <div>
+        <div className="container-fluid">
+          <h1 className='page-header'>Person Management App</h1>
+        </div>
+        <div className='container'>
+          <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+
+          <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+
+          <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+
+          <Person name={this.state.persons[3].name} age={this.state.persons[3].age} />
+
+          <hr />
+
+          <button className='btn btn-danger'>Change State</button>
+        </div>
+      </div>
+    )
+  }
+}
 export default App;
+
+
+// import './App.css';
+// import Person from './Person/Person';
+
+// const App = () => {
+
+//   return (
+//     <div>
+//       <div className="container-fluid">
+//         <h1 className='page-header'>Person Management App</h1>
+//       </div>
+//       <div className='container'>
+//         <Person name='Person - 1' age='22' />
+//         <Person name='Person - 2' age='25' />
+//         <Person name='Person - 3' age='27' />
+//         <Person name='Person - 4' age='26' />
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ src/Person/Person.jsx
@@ -0,0 +1,6 @@
+const Person = (props) => {
+    return(
+        <p className="lead">I am {props.name} and I am {props.age} years old!</p>
+    )
+}
+export default Person;
