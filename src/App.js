96e63252497bccc584eeefb6819b73842981a2b3
arunkudiyal <arunkudiyal@icloud.com>
2022-08-18 11:13:51 +0530

    Conditional Rendering

--- src/App.js
+++ src/App.js
@@ -9,35 +9,84 @@ class App extends Component {
       {name: 'Person - 2', age: 23}, 
       {name: 'Person - 3', age: 24},
       {name: 'Person - 4', age: 25},
-    ]
+    ],
+    showPersons: false
   }
 
   onChangeNameHandler = () => {
     // DONOT MUTATE THE STATES DIRECTLY
-    this.state.persons[1].name = 'Person - 0'
-    this.state.persons[1].age =  '0'
+    // this.state.persons[1].name = 'Person - 0'
+
+    this.setState({
+      persons: [
+        {name: 'Person - One', age: 22}, 
+        {name: 'Person - Two', age: 23}, 
+        {name: 'Person - Three', age: 24},
+        {name: 'Person - Four', age: 25},
+      ]
+    })
+  }
+
+  onClickHandler = () => {
+    console.log('Button 1 was clicked!')
+  }
+
+  onShowPersons = () => {
+    let stateValue = this.state.showPersons
+    this.setState({ showPersons: !stateValue })
+  }
+
+  onSetName = (event) => {
+    this.setState({
+      persons: [
+        {name: 'Person - One', age: 22}, 
+        {name: event.target.value, age: 23}, 
+        {name: 'Person - Three', age: 24},
+        {name: 'Person - Four', age: 25},
+      ]
+    })
   }
 
   render() {
-    // JSX
-    return (
-      <div>
-        <div className="container-fluid">
-          <h1 className='page-header'>Person Management App</h1>
-        </div>
+    // JavaScript
+    let persons = ( <h3>Click on Toggle Persons!</h3> )
+
+    if(this.state.showPersons) {
+      persons = (
         <div className='container'>
-          <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+          <Person 
+            name={this.state.persons[0].name} 
+            age={this.state.persons[0].age} />
 
-          <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+          <Person 
+            name={this.state.persons[1].name} 
+            age={this.state.persons[1].age}
+            changed={(e) => this.onSetName(e)} />
 
-          <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+          <Person 
+            name={this.state.persons[2].name} 
+            age={this.state.persons[2].age} />
 
-          <Person name={this.state.persons[3].name} age={this.state.persons[3].age} />
+          <Person 
+            name={this.state.persons[3].name} 
+            age={this.state.persons[3].age} />
 
           <hr />
 
-          <button className='btn btn-danger'>Change State</button>
+          <button onClick={this.onChangeNameHandler} className='btn btn-danger'>Change State</button>
         </div>
+      )
+    }    
+
+    return (
+      // JSX
+      <div>
+        <div className="container-fluid">
+          <h1>Person Management App</h1>
+          <button onClick={this.onShowPersons} className='btn btn-success'>Toggle Persons</button>
+          <hr />
+        </div>
+        { persons }
       </div>
     )
   }
--- src/Person/Person.jsx
+++ src/Person/Person.jsx
@@ -1,6 +1,12 @@
+import './Person.css'
+
 const Person = (props) => {
-    return(
-        <p className="lead">I am {props.name} and I am {props.age} years old!</p>
+    return (
+        <div id='my-person-div'>
+            <p onClick={props.clicked} className="lead">I am {props.name} and I am {props.age} years old!</p> <br />
+            
+            <input onChange={(e) => props.changed(e)} className='form-control' type="text" placeholder='enter the new name...' />
+        </div>
     )
 }
 export default Person;
